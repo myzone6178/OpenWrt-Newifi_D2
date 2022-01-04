ceba83d426449786605240f40bf560750089aec1
myzone6178 <71270736+myzone6178@users.noreply.github.com>
2022-01-04 23:50:34 +0800

    Update diy-part2.sh

--- diy-part2.sh
+++ diy-part2.sh
@@ -9,7 +9,7 @@
 #============================================================
 
 # Modify default IP
-sed -i 's/192.168.1.1/192.168.5.254/g' package/base-files/files/bin/config_generate
+sed -i 's/192.168.1.1/10.0.0.254/g' package/base-files/files/bin/config_generate
 
 # Modify hostname
 sed -i 's/OpenWrt/Newifi-D2/g' package/base-files/files/bin/config_generate
